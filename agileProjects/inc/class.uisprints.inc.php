9bf49f68b8d8b1b0988fefcd6ac2afc92dcf9dfa
rafael2000 <rafael2000@9f0fd7c0-b776-11de-a66b-f3ad5cb19abf>
2010-11-05 13:09:35 +0000

    Adicionada tela para listar os dados do SprintBacklog

--- agileProjects/inc/class.uisprints.inc.php
+++ agileProjects/inc/class.uisprints.inc.php
@@ -1,7 +1,7 @@
 <?php
         /************************************************************************************************\
         *  Gerencia de projetos ageis                                                                   *
-        *  by Rafael Raymundo da Silva (rafael.silva@serpro.gov.br, rafael2000@gmail.com)               *
+        *  by Rafael Raymundo da Silva (rafael2000@gmail.com)                                           *
         * ----------------------------------------------------------------------------------------------*
         *  This program is free software; you can redistribute it and/or modify it                      *
         *  under the terms of the GNU General Public License as published by the                        *
@@ -9,17 +9,57 @@
         *  option) any later version.                                                                   *
         \***********************************************************************************************/
 
+        include('inc/class.sosprints.inc.php');
+        include('../header.inc.php');
+
         class uisprints{
 
-		function uisprints(){
-			if($_SESSION['phpgw_info']['expresso']['agileProjects']['active'] == NULL){
+                var $listSprints;
+
+                function uisprints(){
+                        if($_SESSION['phpgw_info']['expresso']['agileProjects']['active'] == NULL){
                                 echo "Primeiro selecione um projeto";
                                 exit();
                         }
                         else{
-                                echo("Projeto selecionado eh: ".$_SESSION['phpgw_info']['expresso']['agileProjects']['active']);
-                        }
-		}
+                                $this->listSprints = new sosprints();
+                                $this->listSprints->sosprints();
 
+                                $numBacklogs = count($this->listSprints->sprintsElements['sprints_name']);
+                                echo    "<div><button type=\"button\" onClick=\"javascript:sprintInclude();\">[ ".lang('Include sprint')." ]</button><br/><br/>";
+                                if (count($this->listSprints->sprintsElements) > 0 ){
+                                   echo "
+                                        <table id=\"customers\">
+                                          <thead>
+                                             <tr>
+                                                <th>".lang('Name')."</th>
+                                                <th>".lang('Goal')."</th>
+                                                <th>".lang('Start date')."</th>
+                                                <th>".lang('End date')."</th>
+                                                <th>".lang('Retrospective')."</th>
+						<th>".lang('Actions')."</th>
+                                             </tr>
+                                          </thead>
+                                          <tbody>";
+                                }
+                                for($i=0;$i<$numBacklogs;$i++){
+
+                                                echo    "<tr class=".$line.">
+                                                  <td>".$this->listSprints->sprintsElements['sprints_name'][$i]."</td>
+                                                  <td>".$this->listSprints->sprintsElements['sprints_goal'][$i]."</td>
+                                                  <td>".$this->listSprints->sprintsElements['sprints_dt_start'][$i]."</td>
+                                                  <td>".$this->listSprints->sprintsElements['sprints_dt_end'][$i]."</td>
+						  <td>".$this->listSprints->sprintsElements['sprints_retrospective'][$i]."</td>
+                                                  <td><button type=\"button\" onClick=\"javascript:sprintExecute();\">[ ".lang('Execute sprint')." ]</button></td>
+                                                </tr>";
+                                        if($line == "alt"){
+                                                $line="";
+                                        }else{
+                                                $line="alt";
+                                        }
+                                }
+                                echo "</tbody></table>";
+                        }
+                }
         }
 ?>
